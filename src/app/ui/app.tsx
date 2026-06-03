af116b727fbd5e4b001605ccf27119cac91c7d79
Dastan Hairushev <hairushevdastan@gmail.com>
2026-06-03 18:17:21 +0500

    feat: setup fsd architecture and app entry point

---
+++ src/app/ui/app.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react';
+
+export function App(): ReactNode {
+  return (
+    <div className="app-container">
+      <h1>Pure Forms</h1>
+      <div />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+import { App } from '@/app';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+const rootElement = document.querySelector('#root');
 
-console.log('plug');
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('Failed to find the root element in index.html');
+}
